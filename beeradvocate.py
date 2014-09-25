e63f75f906f04119c289abf9c514b2ff6faf7890
Max <max@recoset.com>
2014-09-25 13:41:13 -0400

    better talk ending

--- beeradvocate.py
+++ beeradvocate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import re
 import csv
 from matplotlib import pyplot as plt
@@ -56,20 +57,51 @@ def cast(df, headers, cast_to):
         df[header] = df[header].astype(cast_to)
 cast(df, ["beer_ABV", "review_appearance", "review_aroma", "review_palate", "review_taste", "review_overall", "review_time"], float)
 
-# plot some things
-df.plot(x="beer_ABV", y="review_overall"); plt.show()
-# too many to plot! take a random sample instead
-random_subsample = random.sample(df.index, 5000)
-df.ix[random_subsample].plot(x="review_overall", y="beer_ABV", ls="", marker="."); plt.show()
+# now let's load all the beers from the former experiment
+with open("beernames.txt", "rb") as f:
+    names = set([line[1] for line in csv.reader(f, delimiter="\t")])
+
+df = df[df.beer_name.isin(names)]
 
 # get the counts of reviews of each beer
-counts = df.groupby("beer_name").size()
+counts = df.groupby("review_profileName").size()
 counts.median()
 counts.mean()
 
 # filter out the useless ones
 # this takes a while, take some questions
-df = df.groupby('beer_beerId').filter(lambda x: len(x) >= 5)
-len(df)
+df = df.groupby('review_profileName').filter(lambda x: len(x) >= 5)
+counts = df2.groupby("review_profileName").size()
+counts.median()
+counts.mean()
 
-# next step would be to vectorize the data, using one-hot vectors for categorical strings
+# plot some things
+df.plot(x="review_overall", y="beer_ABV"); plt.show()
+# obviously the connectedness is screwing things up
+df.plot(x="review_overall", y="beer_ABV", ls="", marker="x"); plt.show()
+# take a random sample instead
+subsample = random.sample(df.index, 500)
+df.ix[subsample].plot(x="review_overall", y="beer_ABV", ls="", marker="x"); plt.show()
+
+# now we want to create the dataset we're actually going to use:
+# profile_name beer_rating1 beer_rating2 ... beer_ratingN
+
+# first get a list of all the profile names to iterate over
+profile_names = sorted(set(df.review_profileName))
+# get a list of all the beer names to iterate over
+beer_names = np.array(sorted(names))
+
+with open("dataset_extension.tsv", "wb") as f:
+    w = csv.writer(f, delimiter="\t")
+    # write the header
+    w.writerow(["profile_name"] + list(beer_names))
+    for profile_name in profile_names:
+        # selct the reviews for this user
+        reviews = df[df.review_profileName == profile_name][["beer_name", "review_overall"]]
+        # put them into an array, to iterate over. This is just because pandas.DataFrame.iterrows
+        #   is also inefficient (allocates a new Series in memory for each row), and more annoying
+        reviews = np.array(reviews)
+        # creates a matrix of ratings, sums them row-wise
+        review_vector = sum([(beer_names == beer_name) * rating for beer_name, rating in reviews])
+        # write the row to disk!
+        w.writerow([profile_name] + list(review_vector))
